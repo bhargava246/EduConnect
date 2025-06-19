086fb93091665918c0428f29c9ec35c397f09a28
Himanshu Bhargava <himanshubhargava246@gmail.com>
2025-06-19 13:29:30 +0530

    jwt ,cookies added
    jwt authentication added , cookie also added for role based access

--- backend/src/controllers/userLogin.js
+++ backend/src/controllers/userLogin.js
@@ -1,17 +1,46 @@
+import bcrypt from "bcryptjs";
 import userModel from "../models/user.js";
 import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
 
 const userLogin = async (req,res)=>{
     const {email,password} = req.body;
+    if(!email || !password)return res.status(400).json({error: 'All fields are mandatory'});
     try {
         const user = await userModel.findOne({email: email});
-        if(password == user.password){
-            res.status(200).json("User Verified Successfully")
+        if(!user){return res.status(401).json({error: 'user does not exist' })}
+        const valid = await bcrypt.compare(password,user.password)
+        const secret = process.env.SECRET
+        if(valid){
+            
+            const token =  jwt.sign(
+                {id: user._id,email},
+                secret,
+                {
+                    expiresIn: "2h"
+                }
+            );
+            const userResponse = {
+                id: user._id,
+                name: user.name,
+                email: user.email, 
+                role: user.role,
+                token
+               };
+            const option = {
+                expires: new Date(Date.now()+3*24*60*60*1000),
+                httpOnly: true
+            };
+            res.status(200).cookie("token",token,option).json({
+                success: true,
+                token,
+                userResponse
+            })
             }
         else{
             res.status(200).json({message: "Incorrect Password"});
     }} catch (error) {
-        res.status(500).json({err: 'user not found'})
+       return res.status(500).json({error: 'internal error'})
     }
 }
 export default userLogin;
--- backend/src/controllers/userRegister.js
+++ backend/src/controllers/userRegister.js
@@ -1,17 +1,43 @@
 import mongoose from "mongoose";
 import userModel from "../models/user.js";
-
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 const userRegister = async (req,res)=>{
     const {name,role,email,password} = req.body;
     if(!name || !role || !email ||!password){
         return res.status(400).send("Information is missing");
     }
+
+   const existUser =  await userModel.findOne({email})
+    if(existUser){
+       return res.status(401).json({error: 'User exist already'})
+    }
+
+    const myEncPassword = await bcrypt.hash(password,10)
     try {
-        const newuserModel = new userModel({name,role,email,password});
+        const newuserModel = new userModel({name,role,email,password:myEncPassword});
         await newuserModel.save();
-        res.status(201).json(newuserModel)
+        const secret = process.env.SECRET
+        if(!secret){return res.status(500).json({error: 'Secret key is missing in env'})}
+       const token =  jwt.sign(
+            {id: newuserModel._id,email},
+            secret,
+            {
+                expiresIn: "2h"
+            }
+        );
+       const userResponse = {
+        id: newuserModel._id,
+        name: newuserModel.name,
+        email: newuserModel.email, 
+        role: newuserModel.role,
+        token
+       };
+       return res.status(201).json(userResponse)
     } 
+
+   
       catch (error) {
     console.error(error);
     res.status(500).json({error: 'failed to store data'});
